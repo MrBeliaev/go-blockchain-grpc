f6c8e8ea9d463c46f7ee2a92ecc3f9ef876f4a87
MrBeliaev <mr.beliaev88@mail.ru>
2022-07-17 11:55:08 +0300

    blockchain

---
+++ chainer/chainer.go
@@ -0,0 +1,43 @@
+package chainer
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+type Block struct {
+	PrvHash string
+	Data    string
+	Hash    string
+}
+
+type BlockChain struct {
+	Blocks []*Block
+}
+
+func MakeBlockChain() *BlockChain {
+	genesisBlock := makeBlock("genesis hash", "genesis data")
+	chain := []*Block{genesisBlock}
+	return &BlockChain{chain}
+}
+
+func (chain *BlockChain) AppendBlock(data string) *Block {
+	prvBlock := chain.Blocks[len(chain.Blocks)-1]
+	newBlock := makeBlock(prvBlock.Hash, data)
+	chain.Blocks = append(chain.Blocks, newBlock)
+	return newBlock
+}
+
+func makeBlock(prvHash string, data string) *Block {
+	block := &Block{
+		PrvHash: prvHash,
+		Data:    data,
+		Hash:    getHash(prvHash, data),
+	}
+	return block
+}
+
+func getHash(prv, data string) string {
+	hash := sha256.Sum256([]byte(prv + data))
+	return hex.EncodeToString(hash[:])
+}
